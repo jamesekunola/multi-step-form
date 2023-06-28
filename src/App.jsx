ad8c75ab3400f17f5e874e75b4a6d6144d38622c
James Ekunola <jamesekunola@gmail.com>
2023-06-28 19:50:17 +0100

    Added CSS styles for desktop screen size optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import FormSteps from "./components/form-steps/FormSteps";
 import Steps from "./components/Steps";
 import { useSelector } from "react-redux";
+import "./App.css";
 
 const App = () => {
   const stepIndex = useSelector((state) => state.steps);
 
   return (
-    <section className={`container ${stepIndex === 4 ? "grid" : "grid-2"}`}>
+    <section className="container">
       <FormSteps stepIndex={stepIndex} />
       <Steps stepIndex={stepIndex} />
     </section>
